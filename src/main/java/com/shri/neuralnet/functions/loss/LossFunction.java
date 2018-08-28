af3435bc655757d1c4ce0e224daefb6f8302231a
Shrinivas Shukla <ssshukla1993@gmail.com>
2018-08-28 23:49:48 +0530

    Added instances of "out-of-the-box" implementations in their respective interfaces

--- src/main/java/com/shri/neuralnet/functions/loss/LossFunction.java
+++ src/main/java/com/shri/neuralnet/functions/loss/LossFunction.java
@@ -1,7 +1,11 @@
 package com.shri.neuralnet.functions.loss;
 
+import com.shri.neuralnet.functions.loss.impl.SquaredErrorFunction;
+
 public interface LossFunction
 {
+
+    LossFunction SQUARED_ERROR = new SquaredErrorFunction();
 
     Double calculate(Double[] expected, Double[] actual);
 
--- src/main/java/com/shri/neuralnet/functions/summing/InputSummingFunction.java
+++ src/main/java/com/shri/neuralnet/functions/summing/InputSummingFunction.java
@@ -3,9 +3,15 @@ package com.shri.neuralnet.functions.summing;
 import java.util.List;
 
 import com.shri.neuralnet.components.Connection;
+import com.shri.neuralnet.functions.summing.impl.WeightedSquaredSumFunction;
+import com.shri.neuralnet.functions.summing.impl.WeightedSumFunction;
 
 public interface InputSummingFunction
 {
+
+    InputSummingFunction WEIGHTED_SUM = new WeightedSumFunction();
+
+    InputSummingFunction WEIGHTED_SQUARED_SUM = new WeightedSquaredSumFunction();
 
     Double calculate(List<Connection> connections);
 
